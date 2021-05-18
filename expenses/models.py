6a33841e7c0800de38df09c14a8e004f525e85f9
mikohan <angara99@gmail.com>
2021-05-18 20:02:38 +0300

    Start crating expenses models

--- expenses/models.py
+++ expenses/models.py
@@ -13,7 +13,15 @@ class Expense(models.Model):
     ]
 
     category = models.CharField(choices=CATEGORY_OPTIONS, max_length=255)
-    description = models.TextField(blank=True, null=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering: ["-updated_at"]
+
+    def __str__(self):
+        return str(self.owner) + "s income"
