170c7c049db721b9d88bc227ca5c1a07a4209be5
mikohan <angara99@gmail.com>
2021-05-18 17:58:42 +0300

    User login stuff working

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import RegisterView, VerifyEmailView
+from .views import LoginAPIView, RegisterView, VerifyEmailView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("activate/", VerifyEmailView.as_view(), name="activate"),
+    path("login/", LoginAPIView.as_view(), name="login"),
 ]
