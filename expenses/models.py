47b0c0e6a295953887c98d7f256487082ba50deb
mikohan <angara99@gmail.com>
2021-05-18 19:34:02 +0300

    Start crating expenses models

--- expenses/models.py
+++ expenses/models.py
@@ -13,6 +13,7 @@ class Expense(models.Model):
     ]
 
     category = models.CharField(choices=CATEGORY_OPTIONS, max_length=255)
+    description = models.TextField(blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False)
