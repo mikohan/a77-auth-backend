568d9e54f3629c33623b9cd9950a655f543a07ac
mikohan <angara99@gmail.com>
2021-05-18 19:34:02 +0300

    Start crating expenses models

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import ExpenseListAPIView, ExpenseDetailAPIView
 
-urlpatters = [
+urlpatterns = [
     path("", ExpenseListAPIView.as_view(), name="expenses"),
-    path("<int:id>/", ExpenseDetailAPIView.as_view(), name="expense"),
+    path("<int:id>", ExpenseDetailAPIView.as_view(), name="expense"),
 ]
