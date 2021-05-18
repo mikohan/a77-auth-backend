ad9269f827eba93c0479c217bbaae52c41571efe
mikohan <angara99@gmail.com>
2021-05-18 15:24:40 +0300

    Verify Email Done

--- authentication/views.py
+++ authentication/views.py
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .utils import Util
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
 
 
 """
@@ -51,3 +53,23 @@ class RegisterView(generics.GenericAPIView):
 class VerifyEmailView(generics.CreateAPIView):
     def get(self, request):
         token = request.GET.get("token")
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY)
+            user = User.objects.get(id=payload["user_id"])
+
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response(
+                {"email": "Successfully activated"}, status=status.HTTP_201_CREATED
+            )
+
+        except jwt.ExpiredSignatureError as indentifier:
+            return Response(
+                {"error": "Activation link Expired"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.DecodeError as indentifier:
+            return Response(
+                {"error": "Invalid token request new one"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
