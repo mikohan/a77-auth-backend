d694c683efdebe6a9fe613c9ce82a55e0f8d9599
mikohan <angara99@gmail.com>
2021-05-18 19:16:00 +0300

    Start crating expenses models

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from expenses.models import Expense
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import ExpenseSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ExpenseListAPIView(ListCreateAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+    queryset = Expense.objects.all()
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+
+class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ExpenseSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    queryset = Expense.objects.all()
+    lookup_field = "id"
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
