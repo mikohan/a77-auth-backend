ae71fe2957344d3a8df92e9dfb8e3b0994635bc0
mikohan <angara99@gmail.com>
2021-05-18 20:02:38 +0300

    Start crating expenses models

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -6,3 +6,8 @@ class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
         fields = ["id", "date", "description", "amount", "category"]
+        extra_kwargs = {
+            "date": {"required": False, "allow_null": True},
+            "amount": {"required": False, "allow_null": True},
+            "category": {"required": False, "allow_null": True},
+        }
