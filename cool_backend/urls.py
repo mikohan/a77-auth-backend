1de831c774cb7ba5597c1afb2f1524a38d3d77d5
mikohan <angara99@gmail.com>
2021-05-18 13:16:41 +0300

    Implement register module

--- cool_backend/urls.py
+++ cool_backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin/", include("authentication.urls")),
+    path("auth/", include("authentication.urls")),
 ]
