677ff31c0903f784446559adeb3d336ad3eb69d5
mikohan <angara99@gmail.com>
2021-05-19 17:47:40 +0300

    Finished with view tests

--- authentication/tests/test_setup.py
+++ authentication/tests/test_setup.py
@@ -1,17 +1,18 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from faker import Faker
 
 
 class TestSetUp(APITestCase):
-    user_data = {
-        "email": "email@gamil.com",
-        "username": "email",
-        "password": "password1234",
-    }
-
     def setUp(self) -> None:
+        self.fake = Faker()
         self.register_url = reverse("register")
         self.login_url = reverse("login")
+        self.user_data = {
+            "email": self.fake.email(),
+            "username": self.fake.email().split("@")[0],
+            "password": hash(self.fake.email),
+        }
 
         return super().setUp()
 
