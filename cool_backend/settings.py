3a0562a8c2ebd084c9654003d355e7d6215e3825
mikohan <angara99@gmail.com>
2021-05-19 09:45:37 +0300

    Start crating expenses models

--- cool_backend/settings.py
+++ cool_backend/settings.py
@@ -147,8 +147,8 @@ SWAGGER_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=300),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
--- expenses/serializers.py
+++ expenses/serializers.py
@@ -6,9 +6,3 @@ class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
         fields = ["id", "date", "description", "amount", "category"]
-        extra_kwargs = {
-            "date": {"required": False, "allow_null": True},
-            "amount": {"required": False, "allow_null": True},
-            "category": {"required": False, "allow_null": True},
-            "description": {"required": False, "allow_null": True},
-        }
