def0ae1391248ffbca221d834eb8c721c6746a31
mikohan <angara99@gmail.com>
2021-05-19 10:31:37 +0300

    Inconve start creation

--- income/admin.py
+++ income/admin.py
@@ -1,3 +1,4 @@
+from income.models import Income
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Income)
--- income/urls.py
+++ income/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import IncomeListAPIView, IncomeDetailAPIView
 
 urlpatterns = [
-    path("", IncomeListAPIView.as_view(), name="income-main"),
+    path("", IncomeListAPIView.as_view(), name="incomes"),
     path("<int:id>", IncomeDetailAPIView.as_view(), name="income"),
 ]
