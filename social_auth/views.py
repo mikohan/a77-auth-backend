3aeb7542537942ed9f2bff059f8d06da7726cdff
mikohan <angara99@gmail.com>
2021-05-20 12:43:07 +0300

    Google authentication start

---
+++ social_auth/views.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from .serializers import GoogleSocialSerializer
+from rest_framework.response import Response
+
+
+class GoogleAuthView(GenericAPIView):
+
+    serializer_class = GoogleSocialSerializer
+
+    def post(self, request):
+        """
+        POST with "auth_token"
+        Send and id_token as from google to get user information
+        """
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data["auth_token"]
+        return Response(data, status=status.HTTP_200_OK)
